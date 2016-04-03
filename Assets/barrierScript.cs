4980a07cbd56edc2552a16fa48c36ed375f525f8
Khanh Nguyen <knguye25@uncc.edu>
2016-04-03 03:35:45 -0400

    added barrier icon and script

---
+++ Assets/barrierScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class barrierScript : MonoBehaviour {
+
+	public Collider2D barrierCollider;
+	public bool isNear;
+
+	// Use this for initialization
+	void Start () {
+		isNear = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isNear && Input.GetKey (KeyCode.X)) {
+			barrierCollider.enabled = false;
+			Debug.Log ("turn off");
+		}
+	}
+	void OnTriggerStay2D(Collider2D other){
+		if(Input.GetKey(KeyCode.X)){
+			barrierCollider.enabled = false;
+			Debug.Log ("turn off");
+		}
+	} 
+	/*
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("enter");
+		if (other.gameObject.tag == "player")
+			isNear = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		Debug.Log ("exit");
+		if (other.gameObject.tag == "player")
+			isNear = false;
+	}
+	*/
+
+}
