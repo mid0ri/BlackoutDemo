0b1d1c6ff94dd45d6886f9a537b762f8806f4710
Locke Ramsey <locke.ramsey@gmail.com>
2016-04-13 20:14:52 -0400

    Added Projectile Enemy
    Projectile doesn't properly shoot from enemy however.

---
+++ Assets/ProjectileEnemyController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileEnemyController : MonoBehaviour
+{
+    //facing variables
+    public GameObject enemyGraphic;
+    bool canFlip = true;
+    bool facingRight = true;
+    float flipTime = 5f;
+    float nextFlipChance = 0f;
+
+    //shooting variables
+    public float shootTime;
+    float startShootTime;
+    bool isShooting;
+    Rigidbody2D enemyRB;
+    public GameObject enemyProjectile;
+    public Transform launchPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextFlipChance)
+        {
+            if (Random.Range(0, 10) >= 5)
+            {
+                flipFacing();
+            }
+            nextFlipChance = Time.time + flipTime;
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (facingRight && other.transform.position.x < transform.position.x)
+            {
+                flipFacing();
+            }
+            else if (!facingRight && other.transform.position.x > transform.position.x)
+            {
+                flipFacing();
+            }
+            canFlip = false;
+            isShooting = true;
+            startShootTime = Time.time + shootTime;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (startShootTime < Time.time)
+            {
+                if (!facingRight)
+                {
+                    Instantiate(enemyProjectile, launchPoint.position, launchPoint.rotation);
+                }
+                else
+                {
+                    Instantiate(enemyProjectile, launchPoint.position, launchPoint.rotation);
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canFlip = true;
+            isShooting = false;
+            enemyRB.velocity = new Vector2(0f, 0f);
+        }
+    }
+
+    void flipFacing()
+    {
+        if (!canFlip) return;
+        float facingX = enemyGraphic.transform.localScale.x;
+        facingX *= -1f;
+        enemyGraphic.transform.localScale = new Vector3(facingX, enemyGraphic.transform.localScale.y, enemyGraphic.transform.localScale.z);
+        facingRight = !facingRight;
+    }
+}
